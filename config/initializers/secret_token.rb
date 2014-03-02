1366849b0289a8d0ccb6683b6ad0cdf1b5af925c
jyli7 <jyl702@gmail.com>
2014-03-02 11:59:04 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheHuddler::Application.config.secret_key_base = 'ca29d73069c0368ead8c2fa2f4445636dccfea2e4fef4ed84b1ad4c6a5e112ff9f9d0e85beb808a0e5a7989571f24030e6124e5c69ee2ac500b03796a30dde4e'
