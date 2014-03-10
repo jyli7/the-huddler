a90a5590ece0279e7c8723ea04379d959072491b
jyli7 <jyl702@gmail.com>
2014-03-10 15:18:14 -0400

    object for endless input

--- app/assets/javascripts/endless_input.js
+++ app/assets/javascripts/endless_input.js
@@ -1,20 +1,44 @@
-$(function () {
-		// Takes in jQuery objects as inputs, returns an object whose
+(function () {
+	// Takes in jQuery objects as inputs, returns an object whose
 	// key is the common value of data-endless, and whose value is
 	// the array of jQuery objects that have that input value
 	var getGroupingsForInputs = function ($inputs) {
-		var result = {};
-		var thisInputValue;
+		var result = {}
+			, inputEndlessValue
+			, $input
+			;
+
 		$.each($inputs, function (i, input) {
-			var $input = $(input);
-			thisInputValue = $input.data('endless');
-			result[thisInputValue] = result[thisInputValue] || [];
-			result[thisInputValue].push($input);
+			$input = $(input);
+			inputEndlessValue = $input.data('endless');
+			result[inputEndlessValue] = result[inputEndlessValue] || [];
+			result[inputEndlessValue].push($input);
 		});
 		return result;
 	};
 
-	var $allRelevantInputs = $('[data-endless]');
-	var uniqueInputGroupings = getGroupingsForInputs($allRelevantInputs);
+	var EndlessInput = function (inputs) {
+		this.inputs = inputs;
+	};
 
-});
+	EndlessInput.prototype.init = function () {
+		console.log(this.inputs);
+	};
+
+	$(function () {
+		var $allRelevantInputs
+			, uniqueInputGroupings
+			,	endlessInput
+			;
+
+		$allRelevantInputs = $('[data-endless]');
+		uniqueInputGroupings = getGroupingsForInputs($allRelevantInputs);
+
+		for (var key in uniqueInputGroupings) {
+			 endlessInput = new EndlessInput(uniqueInputGroupings[key]);
+			 endlessInput.init();
+		}
+	});
+
+	
+}).call(this);
