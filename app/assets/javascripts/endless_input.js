be4066915afb3bb95abbf40b24e9a10c3fa6e4fc
jyli7 <jyl702@gmail.com>
2014-03-10 16:38:20 -0400

    janky scrolling

--- app/assets/javascripts/endless_input.js
+++ app/assets/javascripts/endless_input.js
@@ -26,9 +26,51 @@
 		this.$activeElement = this.$inputs.first();
 	};
 
-	EndlessInput.prototype.activeElement = function () {
+	EndlessInput.prototype.reBlurElements = function () {
+		var majorBlur = 0.4
+			, minorBlur = 0.7
+			, normal = 1
+			, $elementRightBeforeActive
+			, $elementRightAfterActive
+			, $elementTwoAfterActive
+			, $elementsFarBefore
+			, $elementsFarBeyond
+			;
 
-	}
+		// Refactor this into a specific method that takes in DISTANCE FROM ACTIVE! (-1, 0, 1, 2, etc);
+		// Set all inputs to normal first
+		this.$inputs.css('opacity', normal);
+		this.$inputs.show();
+
+		$elementsFarBefore = this.$activeElement.prevAll()
+		if ($elementsFarBefore) {
+			$elementsFarBefore.hide();
+		}
+
+		$elementRightBeforeActive = this.$activeElement.prev();
+		if ($elementRightBeforeActive) {
+			$elementRightBeforeActive.show();
+			$elementRightBeforeActive.css('opacity', majorBlur);
+		}
+
+		// $elementsFarBeyond = this.$activeElement.nextAll()
+		// if ($elementsFarBeyond) {
+		// 	$elementsFarBefore.hide();	
+		// }
+
+		// $elementRightTwoAfterActive = this.$activeElement.next().next();
+		// if ($elementTwoAfterActive) {
+		// 	$elementTwoAfterActive.show();
+		// 	$elementTwoAfterActive.css('opacity', majorBlur);
+		// }
+
+		// $elementRightAfterActive = this.$activeElement.next();
+		// if ($elementRightAfterActive) {
+		// 	$elementRightAfterActive.show();
+		// 	$elementRightAfterActive.css('opacity', minorBlur);
+		// }
+
+	};
 
 	EndlessInput.prototype.activateInput = function ($input) {
 		var distanceToScroll
@@ -40,20 +82,19 @@
 		topOfActiveElement = this.$activeElement.position().top;
 		distanceToScroll = topOfThisElement - topOfActiveElement;
 
+		this.$inputs.css('position', 'relative');
 		this.$inputs.animate({
 			top: "-=" + distanceToScroll
 		}, 400);
 
 		this.$activeElement = $input;
+		this.reBlurElements();
 	};
 
 	EndlessInput.prototype.init = function () {
 		var that = this;
 		// Highlight first element
 		this.$inputs.first().focus();
-
-		// Change position of elements to 'absolute'
-		this.$inputs.css('position', 'relative');
 
 		// Whenever you click into any element, move every element
 		// by the distance between the top element and this element
