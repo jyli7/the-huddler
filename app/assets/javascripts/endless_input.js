25c2841ffb70ab183c741e3a923912221fbfc956
jyli7 <jyl702@gmail.com>
2014-03-11 13:47:30 -0400

    cosmetic cleanup

--- app/assets/javascripts/endless_input.js
+++ app/assets/javascripts/endless_input.js
@@ -2,8 +2,8 @@
 	// HELPER FUNCTIONS
 
 	// Takes in jQuery objects as inputs, returns an object whose
-	// key is the common value of data-endless, and whose value is
-	// the array of jQuery objects that have that input value
+	// key is a particular value for data-endless, and whose value is
+	// the array of jQuery objects that have that same data-endless value
 	var getGroupingsForInputs = function ($inputs) {
 		var result = {}
 			, inputEndlessValue
@@ -19,6 +19,8 @@
 		return result;
 	};
 
+	// Use customHide in place of jQuery's hide for our inputs, to avoid
+	// removal from DOM (which creates funky movement of inputs)
 	var customHide = function ($inputs) {
 		$inputs.animate({
 			opacity: 0
@@ -26,6 +28,7 @@
 		$inputs.addClass('hide-with-z-index');
 	}
 
+	// Use customShow as inverse of customHide
 	var customShow = function ($inputs) {
 		$inputs.animate({
 			opacity: 1
@@ -33,6 +36,7 @@
 		$inputs.removeClass('hide-with-z-index');
 	}
 
+	// Our main constructor function
 	var EndlessInput = function ($inputs) {
 			this.$inputs = $inputs
 		, this.$activeElement = this.$inputs.first()
@@ -180,29 +184,40 @@
 	}
 
 	EndlessInput.prototype.placeArrows = function () {
-		if (this.$upArrow && this.$downArrow) {
+		var topOfVisibles
+			, activeElementIndex
+			, lastVisibleIndex
+			, $lastVisibleEl
+			, bottomOfVisibles
+			, heightOfAllVisibles
+			, midpointOfAllVisibles
+			, upArrowBottom
+			, downArrowTop
+			, inputRightSide
+			;
 
+		if (this.$upArrow && this.$downArrow) {
 			// Find the top/left positions we want to set
 			// We will place arrows in MIDDLE of inputs, and on right hand side
-			var topOfVisibles = this.$activeElement.offset().top;
-			var activeElementIndex = this.$inputs.index(this.$activeElement);
-			var lastVisibleIndex = activeElementIndex + this.visibleInputLength - 1;
-			var $lastVisibleEl = $(this.$inputs.get(lastVisibleIndex));
-			var bottomOfVisibles = $lastVisibleEl.offset().top + $lastVisibleEl.height();
+			topOfVisibles = this.$activeElement.offset().top;
+			activeElementIndex = this.$inputs.index(this.$activeElement);
+			lastVisibleIndex = activeElementIndex + this.visibleInputLength - 1;
+			$lastVisibleEl = $(this.$inputs.get(lastVisibleIndex));
+			bottomOfVisibles = $lastVisibleEl.offset().top + $lastVisibleEl.height();
 			
-			var heightOfAllVisibles = bottomOfVisibles - topOfVisibles;
-			var midpointOfAllVisibles = topOfVisibles + (heightOfAllVisibles / 2);
+			heightOfAllVisibles = bottomOfVisibles - topOfVisibles;
+			midpointOfAllVisibles = topOfVisibles + (heightOfAllVisibles / 2);
 
-			var upArrowBottom = midpointOfAllVisibles -
+			upArrowBottom = midpointOfAllVisibles -
 													this.distanceBetweenArrows / 2;
 			this.$upArrow.css('top', upArrowBottom - this.$upArrow.height());
 
-			var downArrowTop = midpointOfAllVisibles +
+			downArrowTop = midpointOfAllVisibles +
 												 this.distanceBetweenArrows / 2;
 			this.$downArrow.css('top', downArrowTop);
 
 			// We assume inputs are of equal width
-			var inputRightSide = this.$activeElement.offset().left + 
+			inputRightSide = this.$activeElement.offset().left + 
 													 this.$activeElement.width();
 
 			this.$upArrow.css('left', inputRightSide + this.arrowDistancePastInputs);
@@ -227,10 +242,13 @@
 	}
 
 	EndlessInput.prototype.revealArrowsAppropriately = function () {
+		var lastElementIndex
+				;
+
 		this.$upArrow.removeClass('endless-hidden');
 		this.$downArrow.removeClass('endless-hidden');
 
-		var lastElementIndex = this.$inputs.index(this.$inputs.last());
+		lastElementIndex = this.$inputs.index(this.$inputs.last());
 		
 		if (this.$inputs.index(this.$activeElement) === 0) {
 			this.$upArrow.addClass('endless-hidden');
@@ -266,6 +284,8 @@
 		var $allRelevantInputs
 			, uniqueInputGroupings
 			,	endlessInput
+			, arrayOfJqueryObjects
+			, $inputs
 			;
 
 		// Grab all inputs marked with a data-endless value
@@ -276,8 +296,8 @@
 
 		// Initialize endless input for each of those groupings
 		for (var key in uniqueInputGroupings) {
-			var arrayOfJqueryObjects = uniqueInputGroupings[key];
-			var $inputs = $(arrayOfJqueryObjects).map (function () { return this.toArray(); } );
+			arrayOfJqueryObjects = uniqueInputGroupings[key];
+			$inputs = $(arrayOfJqueryObjects).map (function () { return this.toArray(); } );
 			endlessInput = new EndlessInput($inputs);
 			endlessInput.init();
 		}
